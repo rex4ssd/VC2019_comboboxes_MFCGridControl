6098ab10ba969d15570f3727280109b592a9fbb8
R <Mr.R>
2020-07-03 10:17:07 +0800

    init

---
+++ MFC_CustomDlg.h
@@ -0,0 +1,34 @@
+
+// MFC_CustomDlg.h : header file
+//
+
+#pragma once
+
+
+// CMFCCustomDlg dialog
+class CMFCCustomDlg : public CDialogEx
+{
+// Construction
+public:
+	CMFCCustomDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFC_CUSTOM_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
